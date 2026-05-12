e55fe0d4804ff4e56bf1a00b6c611ec676ef77b2
erico-ke <eliasricokergaravat@gmail.com>
2026-05-12 17:16:10 +0200

    CommandHandler.hpp done

--- includes/CommandHandler.hpp
+++ includes/CommandHandler.hpp
@@ -6,7 +6,46 @@
 /*   By: erico-ke <erico-ke@42malaga.student.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 16:46:01 by erico-ke          #+#    #+#             */
-/*   Updated: 2026/05/12 16:46:02 by erico-ke         ###   ########.fr       */
+/*   Updated: 2026/05/12 17:15:47 by erico-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef COMMANDHANDLER_HPP
+# define COMMANDHANDLER_HPP
+
+# include "Client.hpp"
+# include "Channel.hpp"
+# include <string>
+# include <vector>
+# include <iostream>
+
+class Server;
+
+class CommandHandler
+{
+private:
+	static void	handlePass(Client &client, const std::string &params, Server &server);
+	static void	handleNick(Client &client, const std::string &params, Server &server);
+	static void	handleUser(Client &client, const std::string &params, Server &server);
+	static void	handleJoin(Client &client, const std::string &params, Server &server);
+	static void	handlePrivmsg(Client &client, const std::string &params, Server &server);
+	static void	handleKick(Client &client, const std::string &params, Server &server);
+	static void	handleInvite(Client &client, const std::string &params, Server &server);
+	static void	handleTopic(Client &client, const std::string &params, Server &server);
+	static void	handleMode(Client &client, const std::string &params, Server &server);
+	static void	handleQuit(Client &client, const std::string &params, Server &server);
+
+	static void	_tryFinishAuth(Client &client, Server &server);
+
+	static std::string				toUpper(const std::string &s);
+	static std::vector<std::string>	splitParams(const std::string &params);
+public:
+	CommandHandler(void);
+	CommandHandler(const CommandHandler &other);
+	CommandHandler &operator=(const CommandHandler &other);
+	~CommandHandler(void);
+	
+	static void	handle(Client &client, const std::string &line, Server &server);
+};
+
+#endif
