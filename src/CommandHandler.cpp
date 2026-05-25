efd408d836bfab94d3ae5022fd3ccbaff7f46564
erico-ke <eliasricokergaravat@gmail.com>
2026-05-25 15:13:54 +0200

    CommandHandler::privMsg() function done

--- src/CommandHandler.cpp
+++ src/CommandHandler.cpp
@@ -6,7 +6,7 @@
 /*   By: erico-ke <erico-ke@42malaga.student.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 14:07:06 by erico-ke          #+#    #+#             */
-/*   Updated: 2026/05/25 14:30:40 by erico-ke         ###   ########.fr       */
+/*   Updated: 2026/05/25 15:13:21 by erico-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -122,3 +122,45 @@ void	CommandHandler::handleJoin(Client &client, const std::string &params, Serve
 		server.sendToClient(client.getFd(), ":ircserv 332 " + client.getNickname() + " " + chanName + " :" + chan->getTopic() + "\r\n");
 }
 
+void	CommandHandler::handlePrivmsg(Client &client, const std::string &params, Server &server)
+{
+	if (!client.isAuth()) return ;
+	
+	std::vector<std::string> args = splitParams(params);
+	std::string	message = getTrailing(params);
+
+	if (args.empty() || message.empty())
+	{
+		server.sendToClient(client.getFd(), ":ircserv 461 PRIVMSG :Not enough parameters\r\n");
+		return ;
+	}
+	
+	std::string	target = args[0];
+	std::string	fullMsg = client.getPrefix() + " PRIVMSG " + target + " :" + message + "\r\n";
+
+	if (target[0] == '#')
+	{
+		Channel	*chan = server.getChannel(target);
+		if (!chan)
+		{
+			server.sendToClient(client.getFd(), ":ircserv 403 " + target + " :No such channel\r\n");
+			return ;
+		}
+		if (!chan->isMember(&client))
+		{
+			server.sendToClient(client.getFd(), ":ircserv 404 " + target + " :Cannot send to channel (not a member)\r\n");
+			return ;
+		}
+		chan->broadcast(fullMsg, &client);
+	}
+	else
+	{
+		Client	*dest = server.getClientByNick(target);
+		if (!dest)
+		{
+			server.sendToClient(client.getFd(), ":ircserv 401 " + target + " :No such nick\r\n");
+			return ;
+		}
+		server.sendToClient(dest->getFd(), fullMsg);
+	}
+}
