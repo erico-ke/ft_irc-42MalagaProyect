23ca1aadab798403255cb2bcc8c2d5ad9e832154
erico-ke <eliasricokergaravat@gmail.com>
2026-05-25 19:31:38 +0200

    CommandHandler::handleInvite done

--- src/CommandHandler.cpp
+++ src/CommandHandler.cpp
@@ -6,7 +6,7 @@
 /*   By: erico-ke <erico-ke@42malaga.student.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 14:07:06 by erico-ke          #+#    #+#             */
-/*   Updated: 2026/05/25 19:18:49 by erico-ke         ###   ########.fr       */
+/*   Updated: 2026/05/25 19:31:04 by erico-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -203,4 +203,49 @@ void	CommandHandler::handleKick(Client &client, const std::string &params, Serve
 	std::string	kickMsg = client.getPrefix() + " KICK " + chanName + " " + targetNick + " :" + reason + "\r\n";
 	chan->broadcast(kickMsg);
 	chan->removeMember(target);
+}
+
+void CommandHandler::handleInvite(Client& client, const std::string& params, Server& server)
+{
+	if (!client.isAuth()) return;
+
+	std::vector<std::string> args = splitParams(params);
+	if (args.size() < 2)
+	{
+		server.sendToClient(client.getFd(), ":ircserv 461 INVITE :Not enough parameters\r\n");
+    	return;
+	}
+
+	std::string targetNick = args[0];
+	std::string chanName   = args[1];
+
+	Channel* chan = server.getChannel(chanName);
+	if (!chan)
+	{
+		server.sendToClient(client.getFd(), ":ircserv 403 " + chanName + " :No such channel\r\n");
+		return;
+	}
+	if (!chan->isMember(&client))
+	{
+		server.sendToClient(client.getFd(), ":ircserv 442 " + chanName + " :You're not on that channel\r\n");
+		return;
+	}
+	if (chan->isInviteOnly() && !chan->isOperator(&client))
+	{
+		server.sendToClient(client.getFd(), ":ircserv 482 " + chanName + " :You're not channel operator\r\n");
+		return;
+	}
+
+	Client* target = server.getClientByNick(targetNick);
+	if (!target)
+	{
+		server.sendToClient(client.getFd(), ":ircserv 401 " + targetNick + " :No such nick\r\n");
+		return;
+	}
+
+	chan->addInvite(target);
+
+	server.sendToClient(target->getFd(), client.getPrefix() + " INVITE " + targetNick + " :" + chanName + "\r\n");
+
+	server.sendToClient(client.getFd(), ":ircserv 341 " + client.getNickname() + " " + targetNick + " " + chanName + "\r\n");
 }
